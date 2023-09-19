49755aa565494ffe928f4e8754efed4786717eed
gayashan4lk <gayashan4lk@gmail.com>
2023-09-20 01:40:14 +0530

    update options file

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,11 +1,19 @@
 import { NextAuthOptions } from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
+import AzureADB2CProvider from 'next-auth/providers/azure-ad-b2c'
 
 export const options: NextAuthOptions = {
 	providers: [
 		GitHubProvider({
 			clientId: process.env.GITHUB_ID as string,
 			clientSecret: process.env.GITHUB_SECRET as string
+		}),
+		AzureADB2CProvider({
+			tenantId: process.env.AZURE_AD_B2C_TENANT_NAME as string,
+			clientId: process.env.AZURE_AD_B2C_CLIENT_ID as string,
+			clientSecret: process.env.AZURE_AD_B2C_CLIENT_SECRET as string,
+			primaryUserFlow: process.env.AZURE_AD_B2C_PRIMARY_USER_FLOW as string,
+			authorization: { params: { scope: 'offline_access openid' } }
 		})
 	]
 }
